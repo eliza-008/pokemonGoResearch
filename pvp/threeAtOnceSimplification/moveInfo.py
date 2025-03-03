df78ef2918e0a71d5139298e434e4c956c682846
Gianna <gpnicomedes@gmail.com>
2025-03-02 18:46:27 -0800

    update moveInfo.py
    Cleaned up moveInfo.py.

--- pvp/threeAtOnceSimplification/moveInfo.py
+++ pvp/threeAtOnceSimplification/moveInfo.py
@@ -1,25 +1,23 @@
-# generates csv of moves and their characteristics, maybe make a function eventually thatll just grab move data instead of having to manually do it every time?
+# Generates csv of moves and their characteristics
+# Maybe make a function eventually thatll just grab move data instead of having to manually do it every time?
+
 import requests
 from bs4 import BeautifulSoup
 import csv
 
 url = "https://bulbapedia.bulbagarden.net/wiki/List_of_moves_in_Pok%C3%A9mon_GO"
 response = requests.get(url)
-# print(response.status_code) # if getting an error, run this and check response code
-response.raise_for_status()  # Raise an error if request fails
+# print(response.status_code)   # If getting an error, run this and check response code
+response.raise_for_status()     # Raise an error if request fails
 
-# filter data
-soup = BeautifulSoup(response.text, "html.parser")
-tempTable = soup.find_all("table", class_="roundy") # sortable roundy jquery-tablesorter is table class but only roundy works???
+soup = BeautifulSoup(response.text, "html.parser")      # Filter data
+tempTable = soup.find_all("table", class_="roundy")     # Get all tables from URL
 fastMovesTable = None
 
-# Lcate the Fast Moves table
+# Locate the Fast Attacks table
 for table in tempTable:
-    if "Fast Moves" in table.find_previous("title"):
     h2 = table.find_previous("h2")
-    if h2 and h2.find("span", id="Charged_Attacks"):
+    if h2 and h2.find("span", id="Fast_Attacks"):
         fastMovesTable = table
         break
 print(fastMovesTable.prettify())
-
-# 0 clue why not working LOL,
